6a2df54b8e48ad599f718897c6d22f24d0015dd1
Mert Güvençli <guvenclimert@gmail.com>
2021-08-22 19:15:49 +0300

    digit control added

--- src/assets/js/digit.js
+++ src/assets/js/digit.js
@@ -50,3 +50,9 @@ $('.digit-group').find('input').each(function() {
 		}
 	});
 });
+
+const handleOneDigit = (e) => {
+	if (e.value.length > e.maxLength) {
+		e.value = e.value.slice(0, e.maxLength)
+	};
+}
